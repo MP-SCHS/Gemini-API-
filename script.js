5d2b4fac40d35ce7a255c766d0152dd3299e101b
MP-SCHS <28mpucci@salpointe.org>
2026-04-10 14:14:57 -0700

    .env test v4

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const webConsole = document.getElementById("webConsole");
+const G_API = document.getElementById("InputAPI_KEY");
 
 function sendPrompt() {
-    webConsole.innerHTML = env.local.G_API;
+    webConsole.innerHTML = G_API;
 }
