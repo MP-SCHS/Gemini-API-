9ce3830aace0f1dba769206522cfbe4d162a727c
MP-SCHS <28mpucci@salpointe.org>
2026-04-10 16:53:05 -0700

    added AI

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+
+
+import { GoogleGenAI } from "@google/genai";
+const ai = new GoogleGenAI({});
+
+async function main() {
+    const response = await ai.models.generateContent({
+        model: "gemini-3-flash-preview",
+        contents: (document.getElementById("prompt").value),
+    });
+    webConsole.innerHTML = response.text;
+}
+
+
+
 const webConsole = document.getElementById("webConsole");
 const gApiKey = (document.getElementById("InputAPI_KEY").value);
+webConsole.innerHTML = "SavitAIr Response";
 
 function sendPrompt() {
     webConsole.innerHTML = gApiKey;
